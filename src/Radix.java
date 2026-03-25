793fbe98ae5584bea58f9007ed8303b8fa0514c5
JustinHo09 <hojustin09@gmail.com>
2026-03-25 15:22:08 -0500

    I added some more case tests in the main method, and I make mine case sensitive by making seperate buckets for every letter case.
    I added some more test cases three of them are to test case sensitivity for ordering, and one of them is a null test case. Then, since the inputs to test are going to be null or English letters, I removed the non-letter character case. I added seperate buckets for capital characters, which means I had to adjust the number of buckets and fix the character precedence method. Since, the uppercase ones have higher precednce they will now occupy the second to 26th bucket, and lower case ones will take the remaining 26 of 53. In the charPrec method, I made uppercase letters A-Z return precednece integer 1-26 respectivly, and a-z are 27-52 respectivly.

--- src/Radix.java
+++ src/Radix.java
@@ -5,6 +5,25 @@ public class Radix {
         String[] tests = {"google",	"gojo",	"amazingly", "jogo",	"luna",	"pup",	"solas",	"solo",	"pupperino",
                 "amaterasu", "amazon",	"puppy",	"hydra",	"amazonia",	"vueltiao", "ama"};
         stringRadix(tests);
+        System.out.println();
+
+        String[] test2 = {"google",	"gojo",	"amazingly", "Jogo",	"luna",	"pup",	"solas",	"solo",	"pupperino",
+                "amaterasu", "Amazon",	"puppy",	"Hydra",	"amazonia",	"vueltiao", "ama","amazon","amaZingly"};
+        stringRadix(test2);
+        System.out.println();
+
+        String[] test3 = {"Ama","aMa","ama","AMa","aMA","Amb", "AmA"};
+        stringRadix(test3);
+        System.out.println();
+
+        String[] test4 = {};
+        stringRadix(test4);
+        System.out.println();
+
+        String[] test5 = {"Az","AZ","az","aZ","aZa","Aza", "aZA", "azA"};
+        stringRadix(test5);
+        System.out.println();
+
     }
 
     public static void stringRadix(String[] s){
@@ -20,8 +39,8 @@ public class Radix {
             HashMap<Integer,String[]> buckets = new HashMap<>();
 
             // this will be ht HashMap key and is the precedent and ordering of the buckets
-            // 0 = null, 1 = non letter, 2-27 a-z
-            int [] counts = new int[28];
+            // 0 = null, 1-26 A-Z, 27-52 a-z
+            int [] counts = new int[53];
 
             // get the count for each 'current' character for each string
             for(int j = 0; j < s.length;j++){
@@ -33,15 +52,16 @@ public class Radix {
             }
 
             //Intitilizes buckets to be an array of this size.
-            // only 28 becuase there are only 28 options its a letter, 26, non letter 1, or null 1
-            // so total of 28 buckeets.
-            for(int k=0; k <28; k++){
+            // only 53 becuase there are only 53 options its a lowercase
+            // letter, 26, uperecase letter, 26, or null 1
+            // so total of 53 buckeets.
+            for(int k=0; k <counts.length; k++){
                 buckets.put(k,new String[counts[k]]);
             }
 
             // Fill the buckets
             // keep track of the current index of array of the current bucket
-            int[] tempCount = new int [28];
+            int[] tempCount = new int [53];
             int key = 0;
             for(int j = 0; j < s.length; j++){
                 if(i < s[j].length()) {
@@ -56,8 +76,8 @@ public class Radix {
             // Reconstruct the array from the buckets
             // keep track of starting index to replace in original array.
             int reIndex = 0;
-            // only 28 since 28 buckets
-            for(int h=0; h < 28; h++){
+            // only 53 since 53 buckets
+            for(int h=0; h < counts.length; h++){
                 // the bucket has at least an element
                 if(buckets.get(h).length !=0){
                     for(int m = 0; m < buckets.get(h).length; m ++){
@@ -76,59 +96,111 @@ public class Radix {
     }
 
     private static int charPrec(char c){
-        if(c == 'a'){
+        if(c == 'A'){
+            return 1;
+        }else if(c == 'B'){
             return 2;
-        }else if(c == 'b'){
+        }else if(c == 'C'){
             return 3;
-        }else if(c == 'c'){
+        }else if(c == 'D'){
             return 4;
-        }else if(c == 'd'){
+        }else if(c == 'E'){
             return 5;
-        }else if(c == 'e'){
+        }else if(c == 'F'){
             return 6;
-        }else if(c == 'f'){
+        }else if(c == 'G'){
             return 7;
-        }else if(c == 'g'){
+        }else if(c == 'H'){
             return 8;
-        }else if(c == 'h'){
+        }else if(c == 'I'){
             return 9;
-        }else if(c == 'i'){
+        }else if(c == 'J'){
             return 10;
-        }else if(c == 'j'){
+        }else if(c == 'K'){
             return 11;
-        }else if(c == 'k'){
+        }else if(c == 'L'){
             return 12;
-        }else if(c == 'l'){
+        }else if(c == 'M'){
             return 13;
-        }else if(c == 'm'){
+        }else if(c == 'N'){
             return 14;
-        }else if(c == 'n'){
+        }else if(c == 'O'){
             return 15;
-        }else if(c == 'o'){
+        }else if(c == 'P'){
             return 16;
-        }else if(c == 'p'){
+        }else if(c == 'Q'){
             return 17;
-        }else if(c == 'q'){
+        }else if(c == 'R'){
             return 18;
-        }else if(c == 'r'){
+        }else if(c == 'S'){
             return 19;
-        }else if(c == 's'){
+        }else if(c == 'T'){
             return 20;
-        }else if(c == 't'){
+        }else if(c == 'U'){
             return 21;
-        }else if(c == 'u'){
+        }else if(c == 'V'){
             return 22;
-        }else if(c == 'v'){
+        }else if(c == 'W'){
             return 23;
-        }else if(c == 'w'){
+        }else if(c == 'X'){
             return 24;
-        }else if(c == 'x'){
+        }else if(c == 'Y'){
             return 25;
-        }else if(c == 'y'){
+        }else if(c == 'Z'){
             return 26;
-        }else if(c == 'z'){
+        }else if(c == 'a'){
             return 27;
+        }else if(c == 'b'){
+            return 28;
+        }else if(c == 'c'){
+            return 29;
+        }else if(c == 'd'){
+            return 30;
+        }else if(c == 'e'){
+            return 31;
+        }else if(c == 'f'){
+            return 32;
+        }else if(c == 'g'){
+            return 33;
+        }else if(c == 'h'){
+            return 34;
+        }else if(c == 'i'){
+            return 35;
+        }else if(c == 'j'){
+            return 36;
+        }else if(c == 'k'){
+            return 37;
+        }else if(c == 'l'){
+            return 38;
+        }else if(c == 'm'){
+            return 39;
+        }else if(c == 'n'){
+            return 40;
+        }else if(c == 'o'){
+            return 41;
+        }else if(c == 'p'){
+            return 42;
+        }else if(c == 'q'){
+            return 43;
+        }else if(c == 'r'){
+            return 44;
+        }else if(c == 's'){
+            return 45;
+        }else if(c == 't'){
+            return 46;
+        }else if(c == 'u'){
+            return 47;
+        }else if(c == 'v'){
+            return 48;
+        }else if(c == 'w'){
+            return 49;
+        }else if(c == 'x'){
+            return 50;
+        }else if(c == 'y'){
+            return 51;
+        }else if(c == 'z'){
+            return 52;
         }
-        return 1;
+        return -1;
     }
 }
