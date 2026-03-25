05da3e01d8f2693b60aa34d5977c4eee44c10862
JustinHo09 <hojustin09@gmail.com>
2026-03-25 16:25:55 -0500

    I made the logic to check if the three strings make a palindrome triplet.
    I made the logic to check if the three input strings form a palindrome triplet by checking all three of them as if they were one string from left and right to the center. This is done with a two pointer approach to check the left most and right most moving in. Since we are moving in, we also must account for one string ending and moving on to another, which is done in the if cases. Then the index needs to be adjusted to fit the index of the string, since we are not actually concatinating them. Then at the end of each iteration, the left most and right most value are compared to see if they are equal with a method I made, caseAndPair, which just checks that both of them are the same letter regaurdless of case and returns true if so. If at any point the two are not a pair false is returned since it is no longer a palindrome.

--- src/PalindromeTriplet.java
+++ src/PalindromeTriplet.java
@@ -2,6 +2,7 @@ public class PalindromeTriplet {
 
     public static void main(String[] args){
         String[] test = {"race","car","ecar",""};
+
         int [] [] result = tripletIndex(test);
     }
 
@@ -17,11 +18,104 @@ public class PalindromeTriplet {
     }
 
     private static boolean isPalindromeTriplet(String i, String j, String k){
-
+        int left = 0;
+        int right = i.length() + j.length() + k.length()-1;
+        char lefty;
+        char righty;
         // loop throught end and front until they cross, to check if it matches, then
         // return false if they don't at any point
 
+        while(left < right){
+            //find left-most
+            // left-most is at i
+            if(left < i.length()){
+                lefty = i.charAt(left);
+            }else if(left > i.length()-1 && left < i.length()+j.length()){
+                // left is at j and adjust it to the left equivalent of j
+                lefty = j.charAt(left - i.length());
+            }else{
+                // left is at k and adjust it to be the left equivalent of k
+                lefty = k.charAt(left - i.length() - j.length());
+            }
+
+            // find right-most
+            // right-most is at k and adjust it to the right equivalent on k
+            if(i.length()+j.length()-1 < right){
+                righty = k.charAt(right - i.length() - j.length());
+            }else if(i.length()-1 < right){
+                // right at j, adjust right to the right equivalent of j
+                righty = j.charAt(right - i.length());
+            }else{
+                // right at i
+                righty = i.charAt(right);
+            }
+
+            if(!caseAndPair(lefty,righty)){
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
         return true;
+    }
+
+    private static boolean caseAndPair(char first, char second){
+        if((first == 'a' || first == 'A') && (second == 'a' || second == 'A')){
+            return true;
+        }else if((first == 'b' || first == 'B') && (second == 'b' || second == 'B')){
+            return true;
+        }else if((first == 'c' || first == 'C') && (second == 'c' || second == 'C')){
+            return true;
+        }else if((first == 'd' || first == 'D') && (second == 'd' || second == 'D')){
+            return true;
+        }else if((first == 'e' || first == 'E') && (second == 'e' || second == 'E')){
+            return true;
+        }else if((first == 'f' || first == 'F') && (second == 'f' || second == 'F')){
+            return true;
+        }else if((first == 'g' || first == 'G') && (second == 'g' || second == 'G')){
+            return true;
+        }else if((first == 'h' || first == 'H') && (second == 'h' || second == 'H')){
+            return true;
+        }else if((first == 'i' || first == 'I') && (second == 'i' || second == 'I')){
+            return true;
+        }else if((first == 'j' || first == 'J') && (second == 'j' || second == 'J')){
+            return true;
+        }else if((first == 'k' || first == 'K') && (second == 'k' || second == 'K')){
+            return true;
+        }else if((first == 'l' || first == 'L') && (second == 'l' || second == 'L')){
+            return true;
+        }else if((first == 'm' || first == 'M') && (second == 'm' || second == 'M')){
+            return true;
+        }else if((first == 'n' || first == 'N') && (second == 'n' || second == 'N')){
+            return true;
+        }else if((first == 'o' || first == 'O') && (second == 'o' || second == 'O')){
+            return true;
+        }else if((first == 'p' || first == 'P') && (second == 'p' || second == 'P')){
+            return true;
+        }else if((first == 'q' || first == 'Q') && (second == 'q' || second == 'Q')){
+            return true;
+        }else if((first == 'r' || first == 'R') && (second == 'r' || second == 'R')){
+            return true;
+        }else if((first == 's' || first == 'S') && (second == 's' || second == 'S')){
+            return true;
+        }else if((first == 't' || first == 'T') && (second == 't' || second == 'T')){
+            return true;
+        }else if((first == 'u' || first == 'U') && (second == 'u' || second == 'U')){
+            return true;
+        }else if((first == 'v' || first == 'V') && (second == 'v' || second == 'V')){
+            return true;
+        }else if((first == 'w' || first == 'W') && (second == 'w' || second == 'W')){
+            return true;
+        }else if((first == 'x' || first == 'X') && (second == 'x' || second == 'X')){
+            return true;
+        }else if((first == 'y' || first == 'Y') && (second == 'y' || second == 'Y')){
+            return true;
+        }else if((first == 'z' || first == 'Z') && (second == 'z' || second == 'Z')){
+            return true;
+        }
+        return false;
     }
 
 }
