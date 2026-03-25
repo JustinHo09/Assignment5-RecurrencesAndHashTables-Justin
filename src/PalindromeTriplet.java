3b3820548d7bf509450c62d83b72881c785b2b8e
JustinHo09 <hojustin09@gmail.com>
2026-03-25 15:39:35 -0500

    I made the main method with a test case, and I started the logic for the palindrome triplet.
    I made a main method with a test case. Then I mapped out the logic I plan to follow to construct the array to hold the index of the palindrome triplets. Then I made the method for check if the three inputs are a plaindrome triplet by doing the isPalindromeTriplet method, which will iterate across them to check if the three form a palindrome triplet.

--- src/PalindromeTriplet.java
+++ src/PalindromeTriplet.java
@@ -1,3 +1,27 @@
 public class PalindromeTriplet {
-    
+
+    public static void main(String[] args){
+        String[] test = {"race","car","ecar",""};
+        int [] [] result = tripletIndex(test);
+    }
+
+    public static int[][] tripletIndex(String[] input){
+        int rowNum = 0;
+        // check how many triplets are in there to determine the number of rows
+
+        int[][] result = new int[rowNum][3];
+        // go through triplets again to now fill the 2D array
+
+
+        return result;
+    }
+
+    private static boolean isPalindromeTriplet(String i, String j, String k){
+
+        // loop throught end and front until they cross, to check if it matches, then
+        // return false if they don't at any point
+
+        return true;
+    }
+
 }
