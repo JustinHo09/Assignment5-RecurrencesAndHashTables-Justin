ec0c8d5c34603b18e1e27efa5714b550905a104e
JustinHo09 <hojustin09@gmail.com>
2026-03-24 23:33:15 -0500

    I added a main method and started on the main radix sorting method.
    I added a main method which has a test input and started on the radix sorting algorithm. So far I have a for loop at the end to print out the array, then I also have a hashmap and its import to utilize to make buckets.

--- src/Radix.java
+++ src/Radix.java
@@ -1,3 +1,26 @@
+import java.util.HashMap;
 public class Radix {
-    
+
+    public static void main(String[] args){
+        String[] tests = {"google",	"gojo",	"amazingly", "jogo",	"luna",	"pup",	"solas",	"solo",	"pupperino",
+                "amaterasu", "amazon",	"puppy",	"hydra",	"amazonia",	"vueltiao", "ama"};
+        stringRadix(tests);
+    }
+
+    public static void stringRadix(String[] s){
+        HashMap<Integer,String[]> buckets = new HashMap<>();
+        
+
+
+
+
+
+
+
+
+        //prints the sorted array.
+        for(int p = 0; p<s.length; p++){
+            System.out.print(s+",");
+        }
+    }
 }
