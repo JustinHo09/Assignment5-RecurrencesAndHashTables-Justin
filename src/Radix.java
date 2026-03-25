e7a758cf8fb5d05b1dd55047e089320472e32bdd
JustinHo09 <hojustin09@gmail.com>
2026-03-24 23:59:54 -0500

    I made the charcter precedence logic and finished the counting.
    I made character precednece logic, which will help determine the hashmap buckets, so if its null at that position then it will contributing to count of 0. If its a non letter, it will contribute to count 1. If its a letter a-z, it will occupy buckets 2-27 respectivly. The counts index are the buckets keys, and its values at that index are going to be the length of the array inside of that portion bucket.

--- src/Radix.java
+++ src/Radix.java
@@ -23,17 +23,77 @@ public class Radix {
             // 0 = null, 1 = non letter, 2-27 a-z
             int [] counts = new int[28];
 
-            
+            // get the count for each 'current' character for each string
+            for(int j = 0; j < s.length;j++){
+                if(s[j].length() < s.length){
+                    counts[0] = counts[0] +1;
+                }else{
+                    counts[charPrec(s[j].charAt(i))] = counts[charPrec(s[j].charAt(i))]+1;
+                }
+            }
+
         }
-
-
-
-
-
 
         //prints the sorted array.
         for(int p = 0; p<s.length; p++){
             System.out.print(s+",");
         }
+    }
+
+    private static int charPrec(char c){
+        if(c == 'a'){
+            return 2;
+        }else if(c == 'b'){
+            return 3;
+        }else if(c == 'c'){
+            return 4;
+        }else if(c == 'd'){
+            return 5;
+        }else if(c == 'e'){
+            return 6;
+        }else if(c == 'f'){
+            return 7;
+        }else if(c == 'g'){
+            return 8;
+        }else if(c == 'h'){
+            return 9;
+        }else if(c == 'i'){
+            return 10;
+        }else if(c == 'j'){
+            return 11;
+        }else if(c == 'k'){
+            return 12;
+        }else if(c == 'l'){
+            return 13;
+        }else if(c == 'm'){
+            return 14;
+        }else if(c == 'n'){
+            return 15;
+        }else if(c == 'o'){
+            return 16;
+        }else if(c == 'p'){
+            return 17;
+        }else if(c == 'q'){
+            return 18;
+        }else if(c == 'r'){
+            return 19;
+        }else if(c == 's'){
+            return 20;
+        }else if(c == 't'){
+            return 21;
+        }else if(c == 'u'){
+            return 22;
+        }else if(c == 'v'){
+            return 23;
+        }else if(c == 'w'){
+            return 24;
+        }else if(c == 'x'){
+            return 25;
+        }else if(c == 'y'){
+            return 26;
+        }else if(c == 'z'){
+            return 27;
+        }
+        return 1;
     }
 }
